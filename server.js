fd79fb424eed28e5c31a583e62efa0876a7dc24e
nirmalrizal53 <nirmalrijal41@gmail.com>
2016-09-20 21:04:07 +0545

    Initial Commit

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
+var moment = require('moment');
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
+});
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/:id',function(req,res){
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.id)){
+    myDate = moment(req.params.id, "X");
+  } else{
+    myDate = moment(req.params.id, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+        unix: Number(myDate.format("X")),
+        natural: myDate.format("MMMM D, YYYY")
+    });
+  } else{
+    res.json({
+        unix: 'Null',
+        natural: 'Null'
+    });
+  }
+
 });
+
+//View engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
